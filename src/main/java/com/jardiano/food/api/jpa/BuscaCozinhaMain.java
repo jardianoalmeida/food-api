03414184d57c95fa42ce106096b67ee75abe235f
jardiano <jardiano.almeida@gmail.com>
2020-07-12 12:10:43 -0300

    Buscando um objeto pelo id no banco de dados

---
+++ src/main/java/com/jardiano/food/api/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.jardiano.food.api.jpa;
+
+import com.jardiano.food.api.FoodApiApplication;
+import com.jardiano.food.api.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(FoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+    }
+
+}
